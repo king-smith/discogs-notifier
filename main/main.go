2059c7a64a0f4cfe68a15b4c98221a64f4794e16
Mirren King-Smith <king-smith@users.noreply.github.com>
2021-02-14 20:18:06 +1100

    Update main.go

--- main/main.go
+++ main/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -32,26 +29,7 @@ func main() {
 
 	log.Info("Starting notifier")
 
-	go RunNotifier()
-
-	RunServer()
-
-	// if err := notifier.RunNotifier(); err != nil {
-	// 	log.Errorf("Notifier failed: %v", err)
-	// }
-}
-
-func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("OK")
-}
-
-func RunServer() {
-	port := os.Getenv("PORT")
-
-	http.HandleFunc("/health", HealthCheckHandler)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
-
-func RunNotifier() {
-	log.Fatal(notifier.RunNotifier())
+	if err := notifier.RunNotifier(); err != nil {
+		log.Errorf("Notifier failed: %v", err)
+	}
 }
